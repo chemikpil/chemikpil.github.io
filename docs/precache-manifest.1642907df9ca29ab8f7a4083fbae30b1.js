b18f5c4f0a00c7be61a23bb1122ec82ddf5cb2ff
Michał Maćkowiak <mmackowiak@olx.pl>
2018-12-24 16:39:08 +0100

    Prepare build for gh-pages

---
+++ docs/precache-manifest.1642907df9ca29ab8f7a4083fbae30b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d25887007bac220e44",
+    "url": "/static/js/runtime~main.53d25887.js"
+  },
+  {
+    "revision": "c398f3d455f75f876b66",
+    "url": "/static/js/main.c398f3d4.chunk.js"
+  },
+  {
+    "revision": "c669b9ff23a1677ae99d",
+    "url": "/static/js/3.c669b9ff.chunk.js"
+  },
+  {
+    "revision": "f2efcf55d17f97cc626d",
+    "url": "/static/js/2.f2efcf55.chunk.js"
+  },
+  {
+    "revision": "50bea2da1b931cfce527",
+    "url": "/static/js/1.50bea2da.chunk.js"
+  },
+  {
+    "revision": "c398f3d455f75f876b66",
+    "url": "/static/css/main.aad54f8e.chunk.css"
+  },
+  {
+    "revision": "f2efcf55d17f97cc626d",
+    "url": "/static/css/2.859b2338.chunk.css"
+  },
+  {
+    "revision": "50bea2da1b931cfce527",
+    "url": "/static/css/1.56b3d987.chunk.css"
+  },
+  {
+    "revision": "e80f492570ce96b27a5d177e3121ef49",
+    "url": "/index.html"
+  }
+];
